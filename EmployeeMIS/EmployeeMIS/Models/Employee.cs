31400e344947d83f5c7ec2f2447c3983a7c31b11
3dr15 <edrisitprofessional@gmail.com>
2019-10-20 15:59:31 +0530

    Started Working on main Project

---
+++ EmployeeMIS/EmployeeMIS/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EmployeeMIS.Models
+{
+    public class Employee
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public UInt32 PhoneNumber { get; set; }
+        
+        public string DocProofLink { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+
+        public string DepartmentId { get; set; }
+        
+        public UInt32 Salary { get; set; }
+
+    }
+}
---
+++ EmployeeMIS/EmployeeMIS/Models/EmployeeDataBaseSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeMIS.Models
+{
+    public class EmployeeDataBaseSettings
+    {
+        public String EmployeesCollectionName { get; set; }
+
+        public String ConnectionString { get; set; }
+
+        public String DatabaseName { get; set; }
+    }
+
+    public interface IEmployeeDataBaseSettings
+    {
+        String EmployeesCollectionName { get; set; }
+
+        String ConnectionString { get; set; }
+
+        String DatabaseName { get; set; }
+    }
+}
